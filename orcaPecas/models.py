c118207aa16d8468078d7f3972f5d6aa0e5f9d1f
Lucas Farias <lucasamaralfarias1602@gmail.com>
2024-05-06 17:00:53 -0300

    Update models.py

--- orcaPecas/models.py
+++ orcaPecas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class User(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+class Pedidos(models.Model):
+    cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+    nome = models.ForeignKey(Produto, on_delete=models.CASCADE)
+
+class Orcamento(models.Model):
+    produto = models.ManyToManyField(User)
+    preco = models.DecimalField(decimal_places=2, max_digits=9)
