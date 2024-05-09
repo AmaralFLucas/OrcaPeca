e81e607eb65b33d80b3e7f14131d9b971d07d9b5
AmaralFLucas <lucasamaralfarias1602@gmail.com>
2024-05-08 21:20:17 -0300

    Update

--- orcaPecas/views.py
+++ orcaPecas/views.py
@@ -1,32 +1,31 @@
 from django.shortcuts import render, redirect
-from .models import Produtos, Categoria
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
 def solicitar_orcamento(request):
+    pass
+    # if request.method == "POST":
+    #     modelo = request.POST['modelo']
+    #     preco = request.POST['preco']
+    #     descricao = request.POST['descricao']
+    #     quantidade = request.POST['quantidade']
+    #     codigo = request.POST['codigo']
+    #     em_estoque = False
+    #     if int(quantidade) > 0:
+    #         em_estoque = True
+    #     data_criacao = datetime.now()
 
-    if request.method == "POST":
-        modelo = request.POST['modelo']
-        preco = request.POST['preco']
-        descricao = request.POST['descricao']
-        quantidade = request.POST['quantidade']
-        codigo = request.POST['codigo']
-        em_estoque = False
-        if int(quantidade) > 0:
-            em_estoque = True
-        data_criacao = datetime.now()
+    #     Produtos.objects.create(
+    #         nome=nome,
+    #         preço=preco,
+    #         descricao=descricao,
+    #         quantidade=quantidade,
+    #         codigo=codigo,
+    #         em_estoque=em_estoque,
+    #         data_criacao=data_criacao
+    #     )
 
-        Produtos.objects.create(
-            nome=nome,
-            preço=preco,
-            descricao=descricao,
-            quantidade=quantidade,
-            codigo=codigo,
-            em_estoque=em_estoque,
-            data_criacao=data_criacao
-        )
-
-        return redirect('index')
+    #     return redirect('index')
     
-    else:
-        return render(request, 'pages/adicionar_produto.html')
+    # else:
+    #     return render(request, 'pages/adicionar_produto.html')
