4c70f189f44b37369d99ae0ea4df0db7146e3412
Lucas Farias <lucasamaralfarias1602@gmail.com>
2024-05-11 01:33:59 -0300

    Update

--- orcaPecas/urls.py
+++ orcaPecas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('solicitar-orcamento/', views.solicitar_orcamento, name='solicitar-orcamento'),
 ]
--- orcaPecas/views.py
+++ orcaPecas/views.py
@@ -1,32 +1,38 @@
 from django.shortcuts import render, redirect
+from .models import Orcamento
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
+def index(request):
+    return render(request, 'pages/index.html')
+
 def solicitar_orcamento(request):
-    pass
-    # if request.method == "POST":
-    #     modelo = request.POST['modelo']
-    #     preco = request.POST['preco']
-    #     descricao = request.POST['descricao']
-    #     quantidade = request.POST['quantidade']
-    #     codigo = request.POST['codigo']
-    #     em_estoque = False
-    #     if int(quantidade) > 0:
-    #         em_estoque = True
-    #     data_criacao = datetime.now()
-          itens_orcamento = request.POST['teste'].json()
+    if request.method == "POST":
+        nome = request.POST['nome']
+        modelo = request.POST['modelo']
+        sinistro = request.POST['sinistro']
+        oficina = request.POST['oficina']
+        endereco = request.POST['endereco']
+        placa = request.POST['placa']
+        quantidade = request.POST['quantidade']
+        # if int(quantidade) > 0:
+        #     em_estoque = True
+        data_criacao = datetime.now()
+        itens_orcamento = request.POST['peca'].json()
 
-    #     Produtos.objects.create(
-    #         nome=nome,
-    #         preço=preco,
-    #         descricao=descricao,
-    #         quantidade=quantidade,
-    #         codigo=codigo,
-    #         em_estoque=em_estoque,
-    #         data_criacao=data_criacao
-    #     )
+        Orcamento.objects.create(
+            nome=nome,
+            moedlo=modelo,
+            sinistro=sinistro,
+            oficina=oficina,
+            endereco=endereco,
+            placa=placa,
+            itens_orcamento=itens_orcamento,
+            quantidade=quantidade,
+            data_criacao=data_criacao
+        )
 
-    #     return redirect('index')
+        return redirect('index')
     
-    # else:
-    #     return render(request, 'pages/adicionar_produto.html')
+    else:
+        return render(request, 'pages/solicitar_orcamento.html')
