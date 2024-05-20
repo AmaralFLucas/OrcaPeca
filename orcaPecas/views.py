5993263adf6bf6e156de3afe06504eb6812bec4d
AmaralFLucas <lucasamaralfarias1602@gmail.com>
2024-05-20 19:40:12 -0300

    Finalizando o banco

--- orcaPecas/views.py
+++ orcaPecas/views.py
@@ -8,27 +8,28 @@ def index(request):
 
 def solicitar_orcamento(request):
     if request.method == "POST":
-        nome = request.POST['nome']
+        cliente = request.user.id
+        # nome = request.POST['nome']
         modelo = request.POST['modelo']
         sinistro = request.POST['sinistro']
         oficina = request.POST['oficina']
         endereco = request.POST['endereco']
         placa = request.POST['placa']
         quantidade = request.POST['quantidade']
+        peca = request.POST['peca']
         # if int(quantidade) > 0:
         #     em_estoque = True
         data_criacao = datetime.now()
-        itens_orcamento = request.POST['peca'].json()
+        # itens_orcamento = request.POST['peca', 'quantidade'].json()
 
         Orcamento.objects.create(
-            nome=nome,
-            moedlo=modelo,
-            sinistro=sinistro,
-            oficina=oficina,
-            endereco=endereco,
-            placa=placa,
-            itens_orcamento=itens_orcamento,
-            quantidade=quantidade,
+            cliente_id=cliente,
+            modelo=modelo,
+            comentarios_cliente=sinistro,
+            oficina_entrega_nome=oficina,
+            oficina_entrega_endereco=endereco,
+            # placa=placa,
+            itens_orcamento={"peca":peca, "quantidade":quantidade},
             data_criacao=data_criacao
         )
 
